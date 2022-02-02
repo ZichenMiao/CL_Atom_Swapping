4fa2828782fbf76b9fd253bc4fae5a0b3a67010f
Zichen <zichen.miao996@gmail.com>
2022-02-01 21:56:27 -0500

    fix data

--- idatasets/imagenet_100.py
+++ idatasets/imagenet_100.py
@@ -7,7 +7,7 @@ import pdb
 Class_100_List = list(range(100))
 
 
-file_ = open('/home/mzc/Continual_Learning/EFT/Datasets/imagenet100_s1993.txt', 'r')
+file_ = open('Datasets/imagenet100_s1993.txt', 'r')
 Class_100_List_1993 = [f.strip() for f in file_.readlines()]
 
 class ImageNet100(ImageFolder):
